ebbd62ee3d8da3a5163e101a14af839f79ec9070
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-07 14:32:33 -0500

    bookmark model and controller created and working

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+const bookmarksController = require("./controllers/bookmarksController.js")
+
+app.use("/bookmarks", bookmarksController)
 
 app.get("/", (req, res) => {
     res.send("Welcome to Bookmarks App!")
