7c134ccd94c0004dee077c48cf045291ac97305b
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-11 10:16:47 -0500

    added POST route

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express")
 const app = express()
 const bookmarksController = require("./controllers/bookmarksController.js")
 
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log("This code runs for every request")
+    next()
+})
+
 app.use("/bookmarks", bookmarksController)
 
 app.get("/", (req, res) => {
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,6 +1,21 @@
 const express = require("express")
 const bookmarks = express.Router()
 const bookmarksArray = require("../models/bookmark.js")
+const { validateURL } = require("../models/validations.js")
+
+bookmarks.get("/:arrayIndex", (req, res) => {
+    const { arrayIndex } = req.params
+    if(bookmarksArray[arrayIndex]){
+        res.json(bookmarksArray[arrayIndex])
+    } else {
+        res.status(404).json({ error: "Not found" })
+    }
+})
+
+bookmarks.post("/", validateURL, (req, res) => {
+    bookmarksArray.push(req.body)
+    res.json(bookmarksArray[bookmarksArray.length - 1])
+})
 
 bookmarks.get("/", (req, res) => {
     res.json(bookmarksArray)
