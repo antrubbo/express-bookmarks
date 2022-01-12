e575a18b1bdc26bd9dcd88315f3ebbb5ae0e95ef
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-12 10:28:27 -0500

    backend finished

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -3,6 +3,12 @@ const bookmarks = express.Router()
 const bookmarksArray = require("../models/bookmark.js")
 const { validateURL } = require("../models/validations.js")
 
+// bookmarks
+bookmarks.get("/", (req, res) => {
+    res.json(bookmarksArray)
+})
+
+// bookmarks/1
 bookmarks.get("/:arrayIndex", (req, res) => {
     const { arrayIndex } = req.params
     if(bookmarksArray[arrayIndex]){
@@ -12,13 +18,33 @@ bookmarks.get("/:arrayIndex", (req, res) => {
     }
 })
 
+// /bookmarks
 bookmarks.post("/", validateURL, (req, res) => {
     bookmarksArray.push(req.body)
     res.json(bookmarksArray[bookmarksArray.length - 1])
 })
 
-bookmarks.get("/", (req, res) => {
-    res.json(bookmarksArray)
+// /bookmarks/1
+bookmarks.put("/:arrayIndex", validateURL, (req, res) => {
+    const { arrayIndex } = req.params
+    if(bookmarksArray[arrayIndex]){
+        bookmarksArray[arrayIndex] = req.body
+        res.status(200).json(bookmarksArray[arrayIndex])
+    } else {
+        res.redirect("/*")
+    }
 })
+
+// /bookamrks/1
+bookmarks.delete("/:arrayIndex", (req, res) => {
+    const { arrayIndex } = req.params
+    if(bookmarksArray[arrayIndex]){
+        const deletedBookmark = bookmarksArray.splice(arrayIndex, 1)
+        res.status(200).json(deletedBookmark)
+    } else {
+        res.redirect("/*")
+    }
+})
+
 
 module.exports = bookmarks
