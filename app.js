e18294ceb3f534321176c87262b869b94f2fc5bc
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-01-13 12:07:06 -0500

    added description to bookmark model

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const bookmarksController = require("./controllers/bookmarksController.js")
 
 app.use(express.json())
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log("This code runs for every request")
